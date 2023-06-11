d5339ca9ca182f4af2c4863c859c7a3215d7f8d9
Fatma-Badr <85190955+Fatma-Badr@users.noreply.github.com>
2023-06-12 00:25:31 +0300

    Create draw_circles, selected_cell, circles_cor, draw_score, write_turn , screen_reset and draw_resetButton functions

--- frontend.py
+++ frontend.py
@@ -110,3 +110,121 @@ def draw_board(screen):
 
 
 # In[4]:
+
+
+def draw_circles(screen, circles):
+    for circle in circles:
+        
+        if(circle[2]==(3, 0, 0)):
+            s_circle = pygame.draw.circle(screen,(45, 94, 80),(circle[0], circle[1]),23,2)
+        elif (circle[2]==(0, 140, 0)): pass   
+        else:     
+            s_circle = pygame.draw.circle(screen,(94, 96, 95),(circle[0], circle[1]),23)    
+            m_circle = pygame.draw.circle(screen,circle[2],(circle[0], circle[1]),21)
+
+
+# In[5]:
+
+
+circles = [(443, 149,(255, 255, 255)), (495, 45, (0, 0, 0)), (651, 357,(255, 255, 255))]
+
+
+# In[6]:
+
+
+def selected_cell(x, y):
+    col = int((x - board_leftTop_x) / (line + cell_size))
+    row = int((y - board_leftTop_y) / (line + cell_size))
+    return (row, col, player)
+
+
+# In[8]:
+
+
+# (row ,col, 0:B or 1:W)
+def circles_cor(board):
+    circles = []
+    for row in range(len(board)):        
+        for col in range(len(board[row])):
+
+            x = col * (line + cell_size) + cell_size /2 + board_leftTop_x
+
+            y = row * (line + cell_size) + cell_size /2 + board_leftTop_y
+            color=(0, 140, 0)
+            if(board[row][col] == 1): color = (0, 0, 0)
+            elif (board[row][col] == 2): color= (255, 255, 255)
+            elif (board[row][col] == 3): color = (3, 0, 0)    
+            circle = (x, y, color)    
+            circles.append(circle)
+    return circles       
+            
+            
+
+
+# In[9]:
+
+
+sample = [[0,0,1,2,3,0,0,1],
+[1,0,0,0,0,2,3,0],
+[0,0,0,0,0,0,0,0],
+[0,0,0,0,1,0,2,0],
+[1,0,0,0,0,2,0,3],
+[0,0,0,0,0,0,3,0],
+[1,0,0,2,0,0,0,0],
+[0,0,0,0,0,0,3,2]]
+
+
+# In[10]:
+
+
+def draw_score(screen, black_score, white_score):
+    font = pygame.font.Font('freesansbold.ttf', 32)
+    
+    pygame.draw.circle(screen,(0, 0, 0),(100, board_height/2+20-38),23)
+    b_text = font.render(f'Score: {black_score}', True, (255, 255, 255))
+    screen.blit(b_text, dest=(150, board_height/2+20-38-14))
+    
+    pygame.draw.circle(screen,(255, 255, 255),(100, board_height/2+20+38),23)
+    w_text = font.render(f'Score: {white_score}', True, (255, 255, 255))
+    screen.blit(b_text, dest=(150, board_height/2+20+38-14))
+
+
+# In[31]:
+
+
+def write_turn(screen, player):
+    font = pygame.font.Font('freesansbold.ttf', 32)
+
+    s = ""
+    if(player == 1): s="Black"
+    elif(player == 2): s="White"
+    b_text = font.render(f'{s} turn ', True, (255, 255, 255))
+    screen.blit(b_text, dest=(screen_width/2 - 100 , (screen_height-board_height-20)/2 - 16 + board_height + 20))
+
+
+# In[28]:
+
+
+def screen_reset(screen):
+    init_board = [[0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0],
+                  [0,0,0,3,0,0,0,0],
+                  [0,0,3,2,1,0,0,0],
+                  [0,0,0,1,2,3,0,0],
+                  [0,0,0,0,3,0,0,0],
+                  [0,0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0,0]]
+    
+    draw_circles(screen, circles_cor(init_board) )
+
+
+# In[53]:
+
+
+def draw_resetButton(screen):
+    color = (255,255,255)
+    color_light = (100,100,100)
+    pygame.draw.rect(screen,color_light,(150,board_height/2+20+38-14+76,140,40))
+    smallfont = pygame.font.SysFont('freesansbold.ttf',35)
+    text = smallfont.render('Reset' , True , color)
+    screen.blit(text, dest=(186,board_height/2+20+38-14+76+9))

0fa24d28123fc7d777b9e61f0af03deb24bcc6ce
Mamdouh Mekky <mamdouhmekky2001@gmail.com>
2023-06-11 18:14:46 +0300

    minimax function
    This is my turn

---
+++ game/players/minimax.py
@@ -0,0 +1,31 @@
+import copy
+def minimax(player, state):
+    def max_value(state):
+        if len(state.getChildren()) == 0 :
+            return state.heuristic
+        score = float('-inf')
+        new_state = copy.deepcopy(state)
+        new_state = new_state.getChildren()
+        for child in new_state:
+            score = max(score, min_value(new_state)[0])
+        return [score, state.state]
+
+    def min_value(state):
+        if len(state.getChildren()) == 0:
+            return state.heuristic
+        score = float('inf')
+        new_state = copy.deepcopy(state)
+        new_state = new_state.getChildren()
+        for child in new_state:
+            score = min(score, max_value(new_state)[0])
+        return [score, state.state]
+
+    best_move = None
+    best_score = float('-inf')
+    for child in state.getChildren():
+        new_state = copy.deepcopy(state)
+        out= max_value(new_state)
+        if out[0] > best_score:
+            best_score = out[0]
+            best_state = out[1]
+    return best_state
